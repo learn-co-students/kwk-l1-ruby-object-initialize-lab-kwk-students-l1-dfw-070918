eb8a35dd7182c742b280bae7f27fa10118b88713
AnnabelleCat <pikainka@gmail.com>
2018-07-12 21:28:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # dog.rb
+class Dog  #create dog class
+  def initialize(name, breed = "Mutt")  #intialize method has 2 arguments, breed defaults to mutt
+    @name = name
+    @breed = breed
+  end
+  def name  #getter method
+    @name
+  end
+  def breed #getter method
+    @breed
+  end
+end
+
+Dog.new("Fido", "Pug") #creates new instance whose name is Fido and breed is Pug
